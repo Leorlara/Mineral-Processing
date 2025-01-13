d384d28002029c8b44cc3725460ba966c97679b1
Sarfall <leorlara@outlook.com>
2025-01-13 19:06:20 +0200

    modified:   GS and RR equations.py

--- GS and RR equations.py
+++ GS and RR equations.py
@@ -1,3 +1,4 @@
 #GS particle size calculations
 Top_Size_unit=input("Enter the unit of the top size of the particle (mm or µm): ")
 Top_Size=input("Enter the top size of the particle in : ")
+#Is this even working?
