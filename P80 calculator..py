473d4701f6f84cff1f4dfc9e320d581ef705ab88
Sarfall <leorlara@outlook.com>
2025-01-22 19:55:56 +0200

    Finished the linear methood

--- P80 calculator..py
+++ P80 calculator..py
@@ -4,16 +4,15 @@ method = input("Would you like to use a linear of logarithmic method? ").strip()
 
 if method == "linear":
 
-    # The following inputs are mandatory to calculate the P80%
-    sieve_under_linear = float(input("Enter the closest sieve size under 80%: "))
-    sieve_over_linear = float(input("Enter the closest sieve size over 80%: "))
-    cum_passing_over_linear = float(input("Enter the cumulative passing from the size over 80%"))
-    cum_passing_under_linear = float(input("Enter the cumulative passing from the size under 80%"))
+    # Mandatory inputs to calculate the P80%
+    sieve_over_linear = float(input("Enter the closest sieve size above 80%: ")) 
+    sieve_under_linear = float(input("Enter the closest sieve size under 80%: "))  
+    cum_passing_over_linear = float(input("Enter the cumulative passing from the size above 80%: ")) / 100 
+    cum_passing_under_linear = float(input("Enter the cumulative passing from the size under 80%: ")) / 100 
 
-    # The calculations start here
-
-    P80_linear = ((0.8-cum_passing_over_linear)/(cum_passing_under_linear-cum_passing_over_linear))*(sieve_under_linear-sieve_over_linear)+sieve_over_linear
-    print(f"The P80% is {P80_linear}")  
+    # Calculating P80% using linear interpolation
+    P80_linear = ((0.8 - cum_passing_over_linear) / (cum_passing_under_linear - cum_passing_over_linear)) * (sieve_under_linear - sieve_over_linear) + sieve_over_linear
+    print(f"The P80% is {P80_linear}")
 
 elif method == "logarithmic":
 
