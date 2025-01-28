f060048406323d7fb151f7aa3595232e947def86
Sarfall <leorlara@outlook.com>
2025-01-28 22:28:55 +0200

    Added the 4 regression types

--- Flotation fit.py
+++ Flotation fit.py
@@ -3,3 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+while True:
+    regression_type = input("Select regression type add later ").strip().lower() #Regression method to be selected
+
+    if regression_type == "1 comp":
+    elif regression_type == "2 comp":
+    elif regression_type == "3 comp":
+    elif regression_type == "Rectangular":
