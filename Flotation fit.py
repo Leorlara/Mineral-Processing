bebb9d1a0a3b78e6b9b4ad77849b994e16812d19
Sarfall <leorlara@outlook.com>
2025-02-09 21:56:55 +0100

    Added datapoints

--- Flotation fit.py
+++ Flotation fit.py
@@ -7,8 +7,9 @@ while True:
     regression_type = input("Select regression type add later ").strip().lower() #Regression method to be selected
 
     if regression_type == "1 comp":
+        comp1_data_points = int(input("Enter number of data points: ").strip())
         
-
+        
     elif regression_type == "2 comp":
 
     elif regression_type == "3 comp":
